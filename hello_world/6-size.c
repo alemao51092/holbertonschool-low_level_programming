e5d59ba5632d9b09e04c13e09b1f812eb4cd1e0c
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-15 10:50:12 -0800

    c

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - my function
+ *
+ * Description: Print the size of various types on the computer
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  
+    printf("Size of a char: %zu bytes\n", sizeof(char));
+    printf("Size of an: %zu bytes\n", sizeof(int));
+    printf("Size of a long int: %zu bytes\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte\n", sizeof(long long int));
+    printf("Size of a float: %zu byte\n", sizeof(float));
+    
+    return 0;
+}
