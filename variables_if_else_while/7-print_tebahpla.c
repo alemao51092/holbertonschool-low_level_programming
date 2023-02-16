7721a05570467cb320e33a435884858cccddefae
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-16 10:08:03 -0800

    if

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
-
+/**
+ * main - main function 
+ *
+ * Return: 0 
+ */
 
 int main(void)
 {
 	int i;
 
-	for (i = 90; i >= 65; i--)
+	for (i = 122; i >= 97; i--)
 		putchar(i);
 	putchar(10);
 	return (0);
