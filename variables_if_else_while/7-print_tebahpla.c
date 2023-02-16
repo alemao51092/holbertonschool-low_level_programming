242e70ceefab0dd3af4cd16722960606b0dc2261
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-16 10:11:32 -0800

    if

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /**
- * main - main function 
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- * Return: 0 
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int i;
