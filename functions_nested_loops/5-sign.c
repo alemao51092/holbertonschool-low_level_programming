b4adf5fa05a332251820ccf8bdc8532567eb2a81
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 10:17:52 -0800

    l

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,18 +2,18 @@
 #include "main.h"
 /**
  * print_sign - main function
- * @n: number 
+ * @n: number
  * Return: 0
  */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 		putchar (43);
 		return (1);
-	if(n == 0)
+	if (n == 0)
 		putchar (48);
 		return (0);
-	if(n < 0)
+	if (n < 0)
 		putchar (45);
 		return (-1);
 }
