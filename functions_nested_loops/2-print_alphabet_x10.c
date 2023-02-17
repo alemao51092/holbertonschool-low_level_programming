f657ebaa26a37039b8128e3ce1944bcea5b3a4f7
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 08:10:24 -0800

    l

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - main function
+ *Description: Alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar (i);
+		}
+		putchar ('\n');
+	}
+}
