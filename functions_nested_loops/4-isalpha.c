ceb6c9f47a621a7632d867905e81014e3090f12f
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 10:00:22 -0800

    l

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - main function
+ * Description: if is a letter
+ * Return: 0
+ */
+int _isalpha(int c)
+{	
+	if (isalpha(c))
+		return (1);
+	return (0);
+}
