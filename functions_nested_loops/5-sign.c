c9fa878b6e902aa53b160e122e6bc1efa448227d
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 10:20:08 -0800

    l

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,13 +8,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		putchar (43);
 		return (1);
+	}
 	if (n == 0)
+	{
 		putchar (48);
 		return (0);
 	if (n < 0)
+	{
 		putchar (45);
 		return (-1);
+	}
 }
 
