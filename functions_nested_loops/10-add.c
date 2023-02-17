f5bc58093f0a4a63a2b970c653feafe82c39afc0
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 12:21:26 -0800

    l

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 /**
  * add - main function
- * @n: character 
+ * @n: character
+ * @n1: char
+ * @n2: chara
  * Return: 0
- */
+*/
 int add(int n1, int n2)
 {
-	int n = n1+n2;
+	int n = n1 + n2;
+
 	return (n);
 }
 
