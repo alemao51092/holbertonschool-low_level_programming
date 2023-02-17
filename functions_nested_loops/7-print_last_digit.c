bace04ed32404a0c67383f26694ee936f7fbeabc
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-17 11:45:29 -0800

    l

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int print_last_digit = n % 10;
-	putchar(print_last_digit);
-	return (print_last_digit);
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
