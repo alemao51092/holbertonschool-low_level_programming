c494836e2850fb9dfd83a7737d1f9d68d99cec5e
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-19 12:23:14 -0800

    l

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * jack_bauer - main function
+ *  Description: hour minutes
+ */
+
+
+
+void jack_bauer(void)
+{
+	int hour, minute;
+	{
+		for (hour = 0; hour <= 23; hour++)
+			for (minute = 0; minute <= 59; minute++)
+			{
+				putchar((hour / 10) + '0');
+				putchar((hour % 10) + '0');
+				putchar(':');
+				putchar((minute / 10) + '0');
+				putchar((minute % 10) + '0');
+				putchar('\n');
+			}
+	}
+}
