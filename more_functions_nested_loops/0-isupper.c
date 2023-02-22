14796403bd08ad37b037ca18f16589abc69fb2df
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-22 09:57:11 -0800

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - main function
+ * Description: Chek uppercase
+ * @c: character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+		return (0);
+}
