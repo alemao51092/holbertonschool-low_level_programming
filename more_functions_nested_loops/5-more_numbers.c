7794bf09542a51cfaac5a5a0a3bd6b28d177d33d
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-23 06:01:17 -0800

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - main function
+ * @i: charac
+ * Description: Print 10 1 to 14
+ * @n: char
+ * Return: empty
+ */
+void more_numbers(void)
+{
+	int i;
+	int last;
+	int n;
+
+	for (n = 0; n <= 9; i++)
+	{
+		for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+			_putchar ('1');
+		last = i % 10;
+		_putchar (last + '0');
+	}
+	_putchar ('\n');
+}
+}
