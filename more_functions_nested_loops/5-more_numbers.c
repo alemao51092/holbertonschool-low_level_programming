4c2252be92140696592527cb002407b27641da35
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-23 06:08:51 -0800

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,11 +15,12 @@ void more_numbers(void)
 	for (n = 0; n <= 9; i++)
 	{
 		for (i = 0; i <= 14; i++)
+		{
 			if (i > 9)
 				_putchar ('1');
 		last = i % 10;
 		_putchar (last + '0');
-	
+		}
 	_putchar ('\n');
 	}
 }
