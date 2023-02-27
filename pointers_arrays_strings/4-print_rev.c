2b0dedb6bb5d2493055ab1d94fc6457a88ddf391
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-27 11:19:39 -0800

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - main function
+ * Description: ptint
+ * @s: char
+ * Return: empty
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	int x;
+
+	while (*(s + i) != '\0')
+		i++;
+	for (x = i; x >= 0; x--)
+		_putchar (*(s + x));
+	_putchar ('\n');
+}
