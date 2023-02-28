8a8d582c996ed9b37cd79452dace0c17075d1be6
alemao51092 <aarielmartinez1111@gmail.com>
2023-02-28 07:25:37 -0800

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * puts2 - main function
- *
- *
+ * @str: char
+ * Return: empty
+ * Description: % 2
  */
 void puts2(char *str)
-{	
+{
 	int i = 0;
 	int x = 0;
 
@@ -14,7 +15,7 @@ void puts2(char *str)
 		i++;
 	}
 	for (x = 0; x <= i - 1; x++)
-		if (x%2 == 0)
+		if (x % 2 == 0)
 			_putchar (str[x]);
 	_putchar ('\n');
 }
