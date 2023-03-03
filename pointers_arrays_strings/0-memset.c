a130af3611a6588a917d472799dbebb701cc2833
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-03 06:54:06 -0800

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ *_memset - main function
+ * @s: char
+ * @b: char1
+ * @n: char2
+ * Description: fill bytes
+ * Return: j
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; 
+	unsigned int i;
 	char *j = s;
-	
-	
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
-
 	return (j);
 
 }
