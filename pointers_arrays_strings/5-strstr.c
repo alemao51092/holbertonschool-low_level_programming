fa6f265414c0d6e2320028e2fb4298d447a6badb
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-05 11:04:28 -0800

    3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int x;
+	int temp;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+
+		if (kaystack[i] < '\0')
+			temp == haystack[i];
+	for ()
+
+
+
+}
