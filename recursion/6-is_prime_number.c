0d3e4aec8d0164d6a0e9d96bd2170cf88619d3f8
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-06 12:58:33 -0800

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - main function
+ * Description: chek prime number
+ * @n: char
+ * Return: integer
+ */
+
+int
+findPrime(int a, int b)
+{
+	if (a % b == 0 && b < a)
+		return (0);
+	else if (a == b)
+		return (1);
+	else
+		return (findPrime(a, b + 1));
+}
+
+int is_prime_number(int n)
+{
+	int res = 0;
+
+	if (n <= 1)
+		return (0);
+	else if (n > 1)
+		res = findPrime(n, 2);
+	return (res);
+}
