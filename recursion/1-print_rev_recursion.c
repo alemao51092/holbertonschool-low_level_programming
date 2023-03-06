2227604a9addfdaf939661fb2c26b28b919a8b4c
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-06 06:47:47 -0800

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _print_rev_recursion - main function
+* @s: char
+* Description: imprimir al reves
+* Return: empty
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+}
