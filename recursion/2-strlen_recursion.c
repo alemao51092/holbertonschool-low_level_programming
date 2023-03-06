07f60add785053cbaff3f0766fd2b97d7220b341
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-06 09:03:16 -0800

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
  * Return: int
  */
 int _strlen_recursion(char *s)
-{	
-	if (*s == '\0')
-		_strlen_recursion(s + 1);
-
-	return (*s);
-}
+{
+	if (*s != '\0')
+		return 1 + _strlen_recursion(s + 1);
+	else
+		return (0);
+}	
