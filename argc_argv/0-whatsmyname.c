4f499201f2c41d7d063eb714e48a40d2b7a3a5d0
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-07 10:25:08 -0800

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: ch
+ * @argv: ch2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = argc;
+
+	i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
