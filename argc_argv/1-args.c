f94c682237ef3cef996c789f009fda4f87712876
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-07 10:46:01 -0800

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: count argc
+ * @argc: ch
+ * @argv: ch
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
