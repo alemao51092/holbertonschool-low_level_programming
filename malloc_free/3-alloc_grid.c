3028e4c775231823a9a5408e578aa60abf0f4043
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-14 11:54:20 -0700

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
  * Description: grid
  * @width: cha
  * @height: chr
- *
+ * Return: p
  *
  *
  */
@@ -13,22 +13,22 @@ int **alloc_grid(int width, int height)
 	int **p = 0;
 	int i;
 	int x;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
-	p =  malloc(sizeof(int*) * height);
-		
+
+	p =  malloc(sizeof(int *) * height);
+
 		if (p == NULL)
 			return (NULL);
 
 	for (i = 0; i < height; i++)
-	{	
+	{
 		p[i] = malloc(sizeof(int) * width);
-		
+
 		if (p[i] == NULL)
 			return (NULL);
-	
+
 		for (x = 0; x < width; x++)
 			p[i][x] = 0;
 	}
