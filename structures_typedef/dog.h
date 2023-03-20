a20db7cfdb62c30466a2b798b5df1b0ee51c9242
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-20 08:11:42 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef dogh
+#define dogh
+/**
+ * struct dog - main function
+ * @name: ch
+ * @age: ch1
+ * @owner: ch2
+ * Description: structur
+ * Return: empty
+ */
+struct dog
+{
+
+char *name;
+float age;
+char *owner;
+
+
+};
+
+
+
+
+
+#endif
