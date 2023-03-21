f3ce455801427920d896373f3b6c8ff8ea6d309c
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-21 08:29:00 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: function
- * Description: printname
- * Return: Nothing.
- */
+* print_name - prints a name as is
+* @name: name of the person
+* @f: function
+* Description: printname
+* Return: Nothing.
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
