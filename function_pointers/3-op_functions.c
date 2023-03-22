054062839250a321a554408748cd9f5bae1cfb20
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-22 09:08:35 -0700

    3falopa

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <sring.h>
+#include "3-calc.h"
+/**
+ * op_add - main function
+ * Description: add
+ * @a: ch
+ * @b: ch1
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - main function
+ * Description: sub
+ * @a: ch
+ * @b: ch1
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - main function
+ * Description: mul
+ * @a: ch
+ * @b: ch1
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - main function
+ * Description: div
+ * @a: ch
+ * @b: ch1
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - main function
+ * Description: mod
+ * @a: ch
+ * @b: ch1
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
