2a072a0c0dc3347f6650097a11e066691d534b7c
alemao51092 <aarielmartinez1111@gmail.com>
2023-03-22 10:40:49 -0700

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 };
 	int i = 0;
 
-	while (ops[i].f == 0)
+	while (ops[i].f != NULL)
 {
 	if (strcmp(ops[i].f, s) == 0)
 		return (ops[i].f);
---
+++ function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <sring.h>
+#include "3-calc.h"
+int main(int argc, char* argv[])
+{
+	if (argc == 4)
+
+
+
+
+
+
+}
