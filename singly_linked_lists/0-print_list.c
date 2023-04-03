1f625af4a7f2bd4d235ac206904abea931e04c69
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-03 07:04:45 -0700

    0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - main function
+ * Description: print list
+ * @h: ch
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+	{
+			printf("%s %d\n", h->str, h->len);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
