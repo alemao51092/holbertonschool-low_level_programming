ebcbf8f224d51a72a4bd878c8bb8bbcafa7503b6
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-03 08:07:11 -0700

    1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * list_len - main function
+ * Description: numbers of elements
+ * @h: ch
+ * Return: numbers of elements
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
