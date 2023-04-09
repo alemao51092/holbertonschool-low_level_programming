181e85cfbad2460f4ae7a608c6ed9c0b8ab7851a
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-09 14:05:58 -0700

    2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+ 
+ int
+ get_bit(unsigned long int n, unsigned long int index)
+{
+	unsigned int value;
+
+	if (index > 63)
+		return (-1);
+
+	value = (n >> index) & 1;
+
+	return (value);
+}
