b1f3b4ddde2617927462daa25ea68fba2a916232
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-12 09:22:07 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - main function
  * @filename: name of the file
  * @text_content: string to write
- *
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {
