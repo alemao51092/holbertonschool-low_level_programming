0a77511f933b0ddcc64c4f5998bca64b7ec7d82f
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-12 10:12:13 -0700

    2 task betty fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,12 +16,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		return (1);
-	
+
 	while (text_content[i])
 		i++;
 
 	w = write(file, text_content, i);
-	
+
 	if (w == -1)
 		return (-1);
 
