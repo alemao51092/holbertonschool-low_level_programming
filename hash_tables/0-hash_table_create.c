058edbe6f5c04f682f265c1642e99717bed40a4f
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-14 06:39:09 -0700

    0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_creat - main function
+ * @size: table size
+ * Return: t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t;
+	hash_node_t **n;
+
+	t = malloc(sizeof(hash_table_t) * size);
+
+	if (!t)
+		return (NULL);
+
+	n = calloc(size, sizeof(hash_node_t *));
+
+	if (!n)
+	{
+		free(t);
+		return (NULL);
+	}
+
+	t->size = size;
+	t->array = n;
+	return (t);
+
+}
