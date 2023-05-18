d97d1b00b76265b99d89147b1864447cf6a49d8f
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-18 10:19:19 -0700

    task 0

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value);
+
+
+
+
+
+#endif
